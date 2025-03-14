22dc6821dc56f346ff97441f840f5a1e7b2d4110
Sadhana Shashidhar <sadhana@mac.lan>
2025-03-14 23:35:34 +0530

    3nd  modification

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "made some modifications\n";
-  cout << "2nd modification\n";
+  cout << "3rd modification\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
